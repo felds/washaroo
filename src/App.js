f4e4a4344f8a3c8917b819585f77456f0864fd48
Felds Liscia <dev@felds.com.br>
2019-05-01 15:51:20 -0300

    show items and debug data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 import uuid from "uuid/v4";
 
-/**
- * @param {List} list
- */
-const item = list_id => ({
+const list = () => ({
   id: uuid(),
   title: `Nova lista criada em ${Date()}`,
-  list_id
 });
 
-const list = () => ({
-  id: uuid()
+const item = list_id => ({
+  list_id,
+  id: uuid(),
+  title: `Novo item`,
+  count: 1,
 });
 
 const l1 = list();
@@ -19,40 +18,64 @@ const l2 = list();
 const l3 = list();
 const initialData = {
   lists: [l1, l2, l3],
-  items: [item(l2.id), item(l1.id), item(l1.id), item(l1.id)]
+  items: [item(l2.id), item(l1.id), item(l1.id), item(l1.id)],
 };
 
-function List({ list }) {
+function List({ list, items, select }) {
   return (
     <>
-      <p className="list">
+      <p className="list" onClick={select}>
         {list.title}
         <br />
         <small>id: {list.id}</small>
+        <br />
+        {items.length} itens
       </p>
     </>
   );
 }
 
-function Lists({ lists }) {
+function Lists({ lists, items, createList, selectList, unselectList }) {
+  const select = list => () => selectList(list);
+
   return (
     <>
       <h3>Listas:</h3>
-      {lists.map(list => (
-        <List key={list.id} list={list} />
+      {lists.map(l => (
+        <List key={l.id} list={l} items={items.filter(i => i.list_id === l.id)} select={select(l)} />
       ))}
+      <p>
+        <button type="button" onClick={createList}>
+          Nova lista
+        </button>
+        <button type="button" onClick={unselectList}>
+          Des-selecionar
+        </button>
+      </p>
     </>
   );
 }
 
 function App() {
-  const [lists, setLists] = useState([]);
+  const [lists, setLists] = useState(initialData.lists);
+  const [items, setItems] = useState(initialData.items);
+  const [selectedListId, selectListId] = useState();
+
+  const createList = () => setLists([...lists, list()]);
+  const selectList = list => selectListId(list.id);
+  const unselectList = () => selectListId();
 
   return (
     <>
-      <h1>Olar</h1>`
+      <Lists lists={lists} items={items} createList={createList} selectList={selectList} unselectList={unselectList} />
+
+      <DBG {...{ selectedListId, lists, items }} />
     </>
   );
+}
+
+function DBG(data) {
+  return <pre>{JSON.stringify(data, null, 4)}</pre>;
 }
 
 export default App;
