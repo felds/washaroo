2b521ea6f05df84d5caf998f83ae1f17321ec036
Felds Liscia <dev@felds.com.br>
2019-05-01 13:57:07 -0300

    create some dummy lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import uuid from "uuid/v4";
+
+/**
+ * @param {List} list
+ */
+const item = list_id => ({
+  id: uuid(),
+  title: `Nova lista criada em ${Date()}`,
+  list_id
+});
+
+const list = () => ({
+  id: uuid()
+});
+
+const l1 = list();
+const l2 = list();
+const l3 = list();
+const initialData = {
+  lists: [l1, l2, l3],
+  items: [item(l2.id), item(l1.id), item(l1.id), item(l1.id)]
+};
+
+function List({ list }) {
+  return (
+    <>
+      <p className="list">
+        {list.title}
+        <br />
+        <small>id: {list.id}</small>
+      </p>
+    </>
+  );
+}
+
+function Lists({ lists }) {
+  return (
+    <>
+      <h3>Listas:</h3>
+      {lists.map(list => (
+        <List key={list.id} list={list} />
+      ))}
+    </>
+  );
+}
 
 function App() {
+  const [lists, setLists] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Olar</h1>`
+    </>
   );
 }
 
